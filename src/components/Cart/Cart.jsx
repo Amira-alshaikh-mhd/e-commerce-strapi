b1a9f72da6b841f40b5c6073bd685521455d1204
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2023-12-28 01:39:15 +0200

    redux

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import "./Cart.css";
 import { FaShoppingCart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { removeFromCart } from "../../redux/cartReducer";
+import { FaUndo } from "react-icons/fa";
+import { resetCart } from "../../redux/cartReducer";
+
+
+
 
 export default function Cart() {
   const [cartList, setCartList] = useState(false);
@@ -9,54 +17,45 @@ export default function Cart() {
       cartList ? setCartList(false) : setCartList(true);
   };
 
+  const products = useSelector(state=>state.cart.products)
+  const dispatch = useDispatch()
+
+
+  console.log(products)
+
   return (
     <div className="cart">
-      <div className="cart-icon" onClick={showCartList}>
+      <div className="cart-icon" onClick={products.length > 0 && showCartList}>
         <FaShoppingCart />
       </div>
-      <div className="cart-badge">5</div>
+      <div className="cart-badge">{products.length}</div>
 
-      {cartList ? (
+       {cartList || products.length > 0
+       ? (
         <ul className="cart-list">
-          <li className="cart-item">
+
+         {products.map(product =>(
+            <li className="cart-item">
             <img
-              src="http://localhost:1337/uploads/men_01_2cd5ae4273.jpg"
+              src={import.meta.env.VITE_APP_URL + product.image}
               alt=""
               className="cart-item-image"
             />
-            <span className="cart-item-title">classic spring</span>
-            <span className="cart-item-price">120$</span>
+            <span className="cart-item-title">{product.title}</span>
+            <span className="cart-item-price">{product.price}</span>
+            <span className="cart-item-remove"
+            
+            onClick={ ()=>dispatch(removeFromCart({
+              id: product.id,
+            }))}
+
+            ><FaRegTrashAlt /></span>
           </li>
 
-          <li className="cart-item">
-            <img
-              src="http://localhost:1337/uploads/women_02_1af34ae0e0.jpg"
-              alt=""
-              className="cart-item-image"
-            />
-            <span className="cart-item-title">Classic Dress</span>
-            <span className="cart-item-price">45$</span>
-          </li>
-
-          <li className="cart-item">
-            <img
-              src="http://localhost:1337/uploads/kid_01_0ee403960d.jpg"
-              alt=""
-              className="cart-item-image"
-            />
-            <span className="cart-item-title">school collection</span>
-            <span className="cart-item-price">90$</span>
-          </li>
-
-          <li className="cart-item">
-            <img
-              src="http://localhost:1337/uploads/kid_03_04473f9806.jpg"
-              alt=""
-              className="cart-item-image"
-            />
-            <span className="cart-item-title">classic</span>
-            <span className="cart-item-price">50$</span>
-          </li>
+         ))}
+          <span className="cart-reset"
+           onClick={ ()=>dispatch(resetCart())}
+          ><FaUndo /></span>
         </ul>
       ) 
       : ("")
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect, useContext } from 'react'
 import useFetch from '../hooks/useFetch'
 import './Products.css'
 import StoreContext from "../hooks/storeContext";
+import { addToCart } from '../redux/cartReducer';
+import { useDispatch } from 'react-redux';
 
 
 
@@ -12,6 +14,8 @@ export default function Products() {
 
     const [products, setProducts] = useState([])
     const {data, loading, error} = useFetch(filter)
+
+    const dispatch = useDispatch()
 
 useEffect(() =>{
   data &&  setProducts(data)
@@ -28,9 +32,17 @@ useEffect(() =>{
              <div className='product' key={product.id}>
               <h2 className='product-title'>{product.attributes.Title}</h2>
               <div className='product-price'>{product.attributes.Price}</div>
-
               <img className='product-image' src={import.meta.env.VITE_APP_URL + product.attributes.image.data.attributes.url} alt="image" />
               <div className='product-desc'>{product.attributes.Desc}</div>
+              <button className='product-btn'
+              onClick={ ()=>dispatch(addToCart({
+                id: product.id,
+                title: product.attributes.Title,
+                Desc: product.attributes.Desc,
+                price: product.attributes.Price,
+                image: product.attributes.image.data.attributes.url,
+              }))}
+              >Add To Cart</button>
 
              </div>
         ))
